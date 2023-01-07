9b90c234b1d1a89dcab4891717a89dcebfc8dfa4
Joshua "jag" Ginsberg <jag@flowtheory.net>
2023-01-07 15:27:32 -0500

    Slack logging.

--- lnkshrtnr/slack_app.py
+++ lnkshrtnr/slack_app.py
@@ -1,16 +1,19 @@
 import os
 
+import woodchipper
 from slack_bolt import App
 
 from . import repository
 
 app = App(signing_secret=os.getenv("SLACK_SIGNING_SECRET"), token=os.getenv("SLACK_BOT_TOKEN"))
+logger = woodchipper.get_logger(__name__)
 
 
 @app.command("/shortenlink")
 def shorten_link_command(ack, respond, command):
     # Acknowledge command request
     ack()
+    logger.info("Slack /shortenlink command received.", **command)
 
     try:
         # Extract arguments from command
@@ -27,7 +30,9 @@ def shorten_link_command(ack, respond, command):
         repository.create_redirect(code, redirect_to, username, default_parameter)
 
         # Send message indicating success
+        logger.info("Successfully created redirect.", code=code, redirect_to=redirect_to)
         respond(f"Successfully created redirect with code '{code}'")
     except Exception as e:
         # Catch error and send message indicating error and details
+        logger.exception("Error creating redirect.", code=code, redirect_to=redirect_to)
         respond(f"Error: {type(e).__name__}\n{e}")
