d2da3e70a555218a2b2ed927ad60e1ec76da43cc
Joshua "jag" Ginsberg <jag@flowtheory.net>
2023-01-07 15:54:22 -0500

    Try to fix Slack+Flask

--- lnkshrtnr/slack_app.py
+++ lnkshrtnr/slack_app.py
@@ -1,18 +1,26 @@
 import os
 
 import woodchipper
+from flask import current_app
 from slack_bolt import App
 
 from . import repository
+from .database import db
 
 app = App(signing_secret=os.getenv("SLACK_SIGNING_SECRET"), token=os.getenv("SLACK_BOT_TOKEN"))
 logger = woodchipper.get_logger(__name__)
 
 
-@app.command("/shortenlink")
-def shorten_link_command(ack, respond, command):
+def bind_flask_app(context, next):
+    context["flask_app"] = current_app._get_current_object()
+    next()
+
+
+@app.command("/shortenlink", middleware=[bind_flask_app])
+def shorten_link_command(ack, respond, context, command):
     # Acknowledge command request
     ack()
+    context["flask_app"].app_context().push()
     logger.info("Slack /shortenlink command received.", **command)
 
     try:
@@ -28,6 +36,7 @@ def shorten_link_command(ack, respond, command):
 
         # Run create_redirect function
         repository.create_redirect(code, redirect_to, username, default_parameter)
+        db.session.commit()
 
         # Send message indicating success
         logger.info("Successfully created redirect.", code=code, redirect_to=redirect_to)
